eb1e9bc3f785e798c7d7b395b48cc76deb130972
Ram Parshad <163500455+ramparshad@users.noreply.github.com>
2025-07-16 01:28:22 +0530

    Update apiServices.kt

--- MedicalAdminPanel/app/src/main/java/com/example/medicalstoreadmin/data/api/apiServices.kt
+++ MedicalAdminPanel/app/src/main/java/com/example/medicalstoreadmin/data/api/apiServices.kt
@@ -72,7 +72,7 @@ interface apiServices {
         @Field("stock") stock : String
     ) : Response<UpdateProductResponse>?
 
-    // New delete endpoints
+    // New delete 
     @DELETE("deleteAllUsers")
     suspend fun deleteAllUsers(): Response<GenericResponse>?
 
