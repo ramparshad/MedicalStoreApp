a8e7570a3bf93cba106e5751074877ec3d766ccf
Ram Parshad <163500455+ramparshad@users.noreply.github.com>
2025-07-29 02:48:01 +0530

    Update TestCompose.kt

--- MedicalAdminPanel/app/src/main/java/com/example/medicalstoreadmin/TestCompose.kt
+++ MedicalAdminPanel/app/src/main/java/com/example/medicalstoreadmin/TestCompose.kt
@@ -102,7 +102,7 @@ fun MyApp(viewModel: AppViewModel = hiltViewModel() , userID : String? = "" , na
     }
 }
 
-// for preview of composable in
+// for preview of composable 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun test(modifier: Modifier = Modifier) {
