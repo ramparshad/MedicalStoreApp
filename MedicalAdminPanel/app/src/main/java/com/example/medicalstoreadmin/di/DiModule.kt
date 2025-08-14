077463d9cb53bbb1664739d7e7c08907c24b9d1a
Ram Parshad <163500455+ramparshad@users.noreply.github.com>
2025-08-14 23:21:50 +0530

    Update DiModule.kt

--- MedicalAdminPanel/app/src/main/java/com/example/medicalstoreadmin/di/DiModule.kt
+++ MedicalAdminPanel/app/src/main/java/com/example/medicalstoreadmin/di/DiModule.kt
@@ -7,7 +7,7 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-@Modu
+@Modul
 @InstallIn(SingletonComponent::class)
 object DataModule {
 
