ebc01a0d5c424d66cde52e1003bbacb1b0cc0d2b
Ram Parshad <163500455+ramparshad@users.noreply.github.com>
2025-08-15 10:00:52 +0530

    Update addOperation.py

--- MedStoreAPI/db/addOperation.py
+++ MedStoreAPI/db/addOperation.py
@@ -2,7 +2,7 @@ import sqlite3
 import uuid
 from datetime import date
 
-def createUser( name , password , phone_number , email , pinCode , add ):
+def createUser( name , password , phone_number , email , pinCode , addr ):
     
     conn = sqlite3.connect("my_medicalshop.db")
     cursor = conn.cursor()
