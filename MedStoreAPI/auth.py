4476fa3586a85b57b1bb15bb7ed321703bec0b04
Ram Parshad <163500455+ramparshad@users.noreply.github.com>
2025-08-25 22:03:19 +0530

    Update auth.py

--- MedStoreAPI/auth.py
+++ MedStoreAPI/auth.py
@@ -2,7 +2,7 @@ import sqlite3
 
 def user_auth(email, password):
     conn = sqlite3.connect('my_medicalshop.db')
-    cursor = co.cursor()
+    cursor = con.cursor()
 
     cursor.execute('SELECT * FROM Users WHERE email = ? AND password = ?', (email, password))
     user = cursor.fetchone() # fetch whole row details
