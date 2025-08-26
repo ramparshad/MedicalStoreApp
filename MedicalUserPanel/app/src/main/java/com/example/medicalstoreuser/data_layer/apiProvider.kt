1cd620f4a090a1182442afe58cf9b1784100fd84
Ram Parshad <163500455+ramparshad@users.noreply.github.com>
2025-08-26 21:51:16 +0530

    Update apiProvider.kt

--- MedicalUserPanel/app/src/main/java/com/example/medicalstoreuser/data_layer/apiProvider.kt
+++ MedicalUserPanel/app/src/main/java/com/example/medicalstoreuser/data_layer/apiProvider.kt
@@ -11,7 +11,7 @@ object ApiProvider {
         .baseUrl(BASE_URL)
         .client(
             OkHttpClient.Builder()
-            .bu()
+            .bui()
         )
         .addConverterFactory(
             GsonConverterFactory
